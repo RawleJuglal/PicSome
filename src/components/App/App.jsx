745315c667479f5da65106cd10c4159c22d7941b
Rawle Juglal <rawledjuglal@gmail.com>
2023-01-05 11:09:25 -0600

    Set up Pic Some
    Set up with scrimba files for the website

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import './App.css'
+import Header from '../Header/Header'
+import 'remixicon/fonts/remixicon.css'
+
+
+function App() {
+
+  return (
+    <div className="--app-app-container">
+      <Header />
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default App
